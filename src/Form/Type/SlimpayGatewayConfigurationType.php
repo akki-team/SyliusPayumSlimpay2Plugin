67231d0ebdec9fec92a1d287d0d121dcf20133d3
Séverine <severine@akki.fr>
2026-08-14 09:11:07 +0200

    feat: compatibilité sylius 2

--- src/Form/Type/SlimpayGatewayConfigurationType.php
+++ src/Form/Type/SlimpayGatewayConfigurationType.php
@@ -15,7 +15,7 @@ final class SlimpayGatewayConfigurationType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('app_id', TextType::class, [
